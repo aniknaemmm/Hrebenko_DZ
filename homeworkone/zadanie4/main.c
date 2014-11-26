4271d36033de9e11afda90b1484149e33861294f
aniknaemm <aniknaemm@gmail.com>
2014-11-26 21:39:27 +0300

    fix

--- homeworkone/zadanie4/main.c
+++ homeworkone/zadanie4/main.c
@@ -7,6 +7,6 @@ int main()
     printf("Ente four digits:\n");
     scanf("%d%d%d%d",&one,&two,&three,&four);
     printf("Result:\n\n");
-    printf("%d\n %d\n  %d\n\   %d\n",one*2,two*2,three*2,four*2);
+    printf("%d\n %d\n  %d\n   %d\n",one*2,two*2,three*2,four*2);
     return 0;
 }
