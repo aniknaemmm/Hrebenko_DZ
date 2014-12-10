3ea41bbab31a7af1b31f56133595d65eddaa2a7f
aniknaemmm <aniknaemm@gmail.com>
2014-12-10 15:34:43 +0300

    dodelal

--- homeworckfor/zadanie4/main.c
+++ homeworckfor/zadanie4/main.c
@@ -3,8 +3,9 @@
 #include <time.h>
 int main()
 {
-    int secretNumb,interNumb,i=0;
+    int secretNumb,interNumb,i=0,y;
     srand(time(NULL));
+    do{
     secretNumb=rand()%100;
     printf("Hello\nI want to play with you\n");
     do
@@ -23,6 +24,8 @@ int main()
     }
     while(!(secretNumb==interNumb));
     printf("good %c\n my number %d  attempts %d",2,secretNumb,i);
+    printf("esli budesh vvedi 1 esli net to q =");
+    }while(scanf("%d",&y));
 
     return 0;
 }
