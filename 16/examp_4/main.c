4bf65510197e5b03929db54cd063b5d9e2423abf
aniknaemmm <aniknaemm@gmail.com>
2015-02-06 00:57:05 +0300

    brainbrain

--- 16/examp_4/main.c
+++ 16/examp_4/main.c
@@ -6,27 +6,39 @@
 
 int main()
 {
-	char mas[30000];
-	char arr[30000];
-	char stack[30000];
-	int addresBrak[30000];
-	scanf("%s",&mas);
-	int end=-1;
-	for(int i=0;i<30000;i++)arr[i]=0;
-		int k=0;
+    char mas[30000];
+    char arr[30000];
+    char stack[30000];
+    int addresBrak[30000];
+    scanf("%s",&mas);
+    int end=0;
+    for(int i=0; i<30000; i++)arr[i]=0;
+    int k=0;
 
-	for(int i=0;i<strlen(mas);i++)
-	{
+    for(int i=0; i<strlen(mas); i++)
+    {
 
-		switch(mas[i])
-		{
-		case '>':k++;break;
-		case '<':k--;break;
-		case '+':arr[k]++;break;
-		case '-':arr[k]--;break;
-	    case '.':putchar(arr[k]);break;
-	    case ',':arr[k]=getchar();
-        case ';': return 0;
+        switch(mas[i])
+        {
+        case '>':
+            k++;
+            break;
+        case '<':
+            k--;
+            break;
+        case '+':
+            arr[k]++;
+            break;
+        case '-':
+            arr[k]--;
+            break;
+        case '.':
+            putchar(arr[k]);
+            break;
+        case ',':
+            arr[k]=getchar();
+        case ';':
+            return 0;
         case '[':
             if (arr[k]==0)
                 while (arr[k]!=']')
@@ -35,16 +47,16 @@ int main()
             {
                 addresBrak[end]=k;
                 ++end;
-            break;
-        case ']':
-            if (arr[k]!=0)
-                k=addresBrak[end-1];
-            break;
+                break;
+            case ']':
+                if (arr[k]!=0)
+                    k=addresBrak[end-1];
+                break;
+            }
+            ++k;
         }
-        ++k;
     }
-		}
 
 
-	return 0;
+    return 0;
 }
