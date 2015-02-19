8baf1c5fb8f6b00d471189c563d88c8096caaccc
aniknaemmm <aniknaemm@gmail.com>
2015-02-19 17:35:39 +0000

    3-0

---
+++ 18/TicTacToe/TTTxo.h
@@ -0,0 +1,7 @@
+#ifndef TTTXO_H_INCLUDED
+#define TTTXO_H_INCLUDED
+#define SIZE 4
+void tttOxShow(int ttt[][SIZE][SIZE]);
+void playTTTox(int ttt[][SIZE][SIZE]);
+
+#endif // TTTXO_H_INCLUDED
---
+++ 18/TicTacToe/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TTTxo.h"
+
+#define SIZE 4
+
+int main()
+{
+    printf("Hello world!TTTxo\n");
+
+    int ttt[SIZE][SIZE][SIZE]={{{0}}};
+
+
+    tttOxShow(ttt);
+    playTTTox(ttt);
+tttOxShow(ttt);
+
+    playTTTox(ttt);
+tttOxShow(ttt);
+
+
+
+    return 0;
+}
