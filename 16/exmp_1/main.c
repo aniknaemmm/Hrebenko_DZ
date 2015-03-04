aab4a7f378e3fc57b0e857a46670d14ca003ab8e
aniknaemmm <aniknaemmm@gmail.com>
2015-03-04 03:09:49 +0300

    hz

--- 16/exmp_1/main.c
+++ 16/exmp_1/main.c
@@ -4,27 +4,46 @@
 #define sizennn 300
 int main()
 {
-    char you[sizennn];
-    char stack[sizennn];
+    char *you=NULL;
+    char *stack=NULL;
+    you=(char *)malloc(sizennn*sizeof(char));
+    stack=(char *)malloc(sizennn*sizeof(char));
+    if(you==NULL||stack==NULL)
+    {
+        fprintf(stderr,"memory is full");
+        exit(1);
+    }
+    int ziz=0;
     int k=-1;
     printf("hello enter you skobki!\n");
-    scanf("%s",&you);
-    for(int i=0; i<strlen(you); i++)
+
+    do
+    {
+        you[ziz]=getchar();
+    }
+    while(you[ziz++]=='\n');
+
+    for(int i=0; i<ziz; i++)
     {
         if(you[i]=='['||you[i]=='('||you[i]=='{')
         {
             stack[++k]=you[i];
         }
-        else if(you[i]=='}'||you[i]==')'||you[i]==']')
+        else if((you[i]=='}'&&stack[k]!='{')||(you[i]==']'&&stack[k]!='[')||(you[i]==')'&&stack[k]!='(')||(i==ziz-1&&k!=-1))
         {
-            if(k==-1)
-            {
-                printf("nepravel'no rastavlenni skobki");
-                return 0;
-            }
-           stack[k--];
+            printf("eror");
+            return 0;
         }
+        else
+        {
+            k--;
+        }
+
     }
-    printf("skobki stoi9t pravilno");
+    printf("correctly brackets ");
+    free(you);
+    free(stack);
     return 0;
 }
+
+
