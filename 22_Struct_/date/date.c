4f58461aab55a214abc6c48d14bf121f8b6cfa84
aniknaemmm <aniknaemmm@gmail.com>
2015-03-05 21:17:24 +0300

    ff

--- 22_Struct_/date/date.c
+++ 22_Struct_/date/date.c
@@ -1,11 +1,11 @@
 #include "date.h"
 
-void enterDate(Date *data){
+/*void enterDate(Date *data){
 
     printf("ente date format gg.mm.dd");
     scanf("%d.%d.%d",data->year,data->month,data->day);
 
-}
+}*/
 
 void convDate(Date *data){
     if((data->year%400==0)&&(data->year%4==0)&&(data->year%100!=0)){
