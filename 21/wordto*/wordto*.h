e1499e25cf289fb627241f19bb3d008f958bddd9
aniknaemmm <aniknaemm@gmail.com>
2015-03-07 23:40:35 +0300

    correct

--- 21/wordto*/wordto*.h
+++
@@ -1,7 +0,0 @@
-#ifndef WORDTO_H_INCLUDED
-#define WORDTO_H_INCLUDED
-
-char *searchProstSlov(const char *stringOne,char *bufer);
-int chekProstoe(char *stringone);
-void zamenaSlov(char *stringOne,char*bufer);
-#endif // WORDTO*_H_INCLUDED
