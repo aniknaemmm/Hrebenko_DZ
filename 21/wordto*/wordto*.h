2b7059a998de1294af48337c175477fba2cb9830
aniknaemmm <aniknaemmm@gmail.com>
2015-03-08 03:13:34 +0300

    refresh

---
+++ 21/wordto*/wordto*.h
@@ -0,0 +1,7 @@
+#ifndef WORDTO_H_INCLUDED
+#define WORDTO_H_INCLUDED
+
+char *searchProstSlov(const char *stringOne,char *bufer);
+int chekProstoe(char *stringone);
+void zamenaSlov(char *stringOne,char*bufer);
+#endif // WORDTO*_H_INCLUDED
