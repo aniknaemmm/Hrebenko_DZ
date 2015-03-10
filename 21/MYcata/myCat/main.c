1b5426a1750ba710c72d7e4b49407c3213a115c4
aniknaemmm <aniknaemm@gmail.com>
2015-03-10 14:39:59 +0000

    f

--- 21/MYcata/myCat/main.c
+++ 21/MYcata/myCat/main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 int main(void)
 {
-    getchar();
-
-    for(int i=0;i<10;i++){
-        printf("%d",i);
-    }
-
     printf("Hello World!\n");
     return 0;
 }
