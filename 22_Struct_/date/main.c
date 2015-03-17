c56d817726f0ddd2a1c0e008a447a58497601c48
aniknaemmm <aniknaemm@gmail.com>
2015-03-17 16:31:39 +0000

    normaldate

--- 22_Struct_/date/main.c
+++ 22_Struct_/date/main.c
@@ -2,15 +2,50 @@
 
 int main(void)
 {
-    Date youDate, tempDate;
-
-    enterDate(&youDate);
-    showDate(youDate);
+    Date youDate={1,1,1}, tempDate;
     tempDate=youDate;
-    addingDay(&tempDate);
-     showDate(tempDate);
-     printf("\n\nffff -%d\n\n",diffDateDay(youDate,tempDate));
+     int chois=0;
 
-    return 0;
+     while (!(chois=!7)) {
+
+         printf("instruction\n");
+         printf("menu\n1.Show you first date\n2.Show you next date\n3.adding day\n4.ustonovit' tekushuiu datu dlia otschiota\n"
+                "5.enter you date\n6.diff\n");
+         scanf("%d",&chois);
+         system("clear");
+         switch (chois) {
+         case 1:
+             showDate(youDate);
+             break;
+         case 2:
+             showDate(tempDate);
+             break;
+         case 3:
+             addingDay(&tempDate);
+             break;
+         case 4:
+             youDate=tempDate;
+             break;
+         case 5:
+             enterDate(&youDate);
+             tempDate=youDate;
+             break;
+         case 6:
+             printf("diff day %d\n",diffDateDay(youDate,tempDate));
+             break;
+         case 7:
+             printf("\end.\n");
+             break;
+
+         default:
+             printf("\ninvalid value\n");
+             exit(1);
+             break;
+         }
+
+
+     }
+
+   return 0;
 }
 
